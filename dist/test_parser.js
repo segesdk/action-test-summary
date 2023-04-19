4ffe6747216280b67f97c5ff3104ca9bdb2f230f
Michael Christensen <mac@seges.dk>
2023-04-19 15:31:02 +0200

    seges-dist

---
+++ dist/test_parser.js
@@ -0,0 +1,294 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.parseFile = exports.parseJunitFile = exports.parseTapFile = exports.parseJunit = exports.parseTap = exports.TestStatus = void 0;
+const fs = __importStar(require("fs"));
+const util = __importStar(require("util"));
+const xml2js_1 = __importDefault(require("xml2js"));
+var TestStatus;
+(function (TestStatus) {
+    TestStatus[TestStatus["Pass"] = 1] = "Pass";
+    TestStatus[TestStatus["Fail"] = 2] = "Fail";
+    TestStatus[TestStatus["Skip"] = 4] = "Skip";
+})(TestStatus = exports.TestStatus || (exports.TestStatus = {}));
+function parseTap(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const lines = data.trim().split(/\r?\n/);
+        let version = 12;
+        let header = 0;
+        let trailer = false;
+        if (lines.length > 0 && lines[header].match(/^TAP version 13$/)) {
+            version = 13;
+            header++;
+        }
+        if (lines.length > 0 && lines[header].match(/^1\.\./)) {
+            // TODO: capture the plan for validation
+            header++;
+        }
+        let testMax = 0;
+        let num = 0;
+        const suites = [];
+        let exception = undefined;
+        let cases = [];
+        let suitename = undefined;
+        const counts = {
+            passed: 0,
+            failed: 0,
+            skipped: 0
+        };
+        for (let i = header; i < lines.length; i++) {
+            const line = lines[i];
+            let found;
+            let status = TestStatus.Skip;
+            let name = undefined;
+            let description = undefined;
+            let details = undefined;
+            if (found = line.match(/^\s*#(.*)/)) {
+                if (!found[1]) {
+                    continue;
+                }
+                /* a comment starts a new suite */
+                if (cases.length > 0) {
+                    suites.push({
+                        name: suitename,
+                        cases: cases
+                    });
+                    suitename = undefined;
+                    cases = [];
+                }
+                if (suitename)
+                    suitename += " " + found[1].trim();
+                else
+                    suitename = found[1].trim();
+                continue;
+            }
+            else if (found = line.match(/^ok(?:\s+(\d+))?\s*-?\s*([^#]*?)\s*#\s*[Ss][Kk][Ii][Pp]\S*(?:\s+(.*?)\s*)?$/)) {
+                num = parseInt(found[1]);
+                status = TestStatus.Skip;
+                name = (found[2] && found[2].length > 0) ? found[2] : undefined;
+                description = found[3];
+                counts.skipped++;
+            }
+            else if (found = line.match(/^ok(?:\s+(\d+))?\s*-?\s*(?:(.*?)\s*)?$/)) {
+                num = parseInt(found[1]);
+                status = TestStatus.Pass;
+                name = found[2];
+                counts.passed++;
+            }
+            else if (found = line.match(/^not ok(?:\s+(\d+))?\s*-?\s*([^#]*?)\s*#\s*[Tt][Oo][Dd][Oo](?:\s+(.*?)\s*)?$/)) {
+                num = parseInt(found[1]);
+                status = TestStatus.Skip;
+                name = (found[2] && found[2].length > 0) ? found[2] : undefined;
+                description = found[3];
+                counts.skipped++;
+            }
+            else if (found = line.match(/^not ok(?:\s+(\d+))?\s*-?\s*-?\s*(?:(.*?)\s*)?$/)) {
+                num = parseInt(found[1]);
+                status = TestStatus.Fail;
+                name = found[2];
+                counts.failed++;
+            }
+            else if (line.match(/^Bail out\!/)) {
+                const message = (line.match(/^Bail out\!(.*)/));
+                if (message) {
+                    exception = message[1].trim();
+                }
+                break;
+            }
+            else if (line.match(/^$/)) {
+                continue;
+            }
+            else if (line.match(/^1\.\.\d+/)) {
+                // TODO: capture the plan for validation
+                trailer = true;
+                continue;
+            }
+            else {
+                throw new Error(`unknown TAP line ${i + 1}: '${line}'`);
+            }
+            if (isNaN(num)) {
+                num = ++testMax;
+            }
+            else if (num > testMax) {
+                testMax = num;
+            }
+            if ((i + 1) < lines.length && lines[i + 1].match(/^  ---$/)) {
+                i++;
+                while (i < lines.length && !lines[i + 1].match(/^  \.\.\.$/)) {
+                    const detail = (lines[i + 1].match(/^  (.*)/));
+                    if (!detail) {
+                        throw new Error("invalid yaml in test case details");
+                    }
+                    if (details)
+                        details += "\n" + detail[1];
+                    else
+                        details = detail[1];
+                    i++;
+                }
+                if (i >= lines.length) {
+                    throw new Error("truncated yaml in test case details");
+                }
+                i++;
+            }
+            if (trailer) {
+                throw new Error("unexpected test results after trailer");
+            }
+            cases.push({
+                status: status,
+                name: name,
+                description: description,
+                details: details
+            });
+        }
+        suites.push({
+            name: suitename,
+            cases: cases
+        });
+        return {
+            counts: counts,
+            suites: suites,
+            exception: exception
+        };
+    });
+}
+exports.parseTap = parseTap;
+function parseJunitXml(xml) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let testsuites;
+        if ('testsuites' in xml) {
+            testsuites = xml.testsuites.testsuite || [];
+        }
+        else if ('testsuite' in xml) {
+            testsuites = [xml.testsuite];
+        }
+        else {
+            throw new Error("expected top-level testsuites or testsuite node");
+        }
+        if (!Array.isArray(testsuites)) {
+            throw new Error("expected array of testsuites");
+        }
+        const suites = [];
+        const counts = {
+            passed: 0,
+            failed: 0,
+            skipped: 0
+        };
+        for (const testsuite of testsuites) {
+            const cases = [];
+            if (!Array.isArray(testsuite.testcase)) {
+                continue;
+            }
+            for (const testcase of testsuite.testcase) {
+                let status = TestStatus.Pass;
+                const id = testcase.$.id;
+                const classname = testcase.$.classname;
+                const name = testcase.$.name;
+                const duration = testcase.$.time;
+                let details = undefined;
+                if (testcase.skipped) {
+                    status = TestStatus.Skip;
+                    counts.skipped++;
+                }
+                else if (testcase.failure || testcase.error) {
+                    status = TestStatus.Fail;
+                    details = (testcase.failure || testcase.error)[0]._;
+                    counts.failed++;
+                }
+                else {
+                    counts.passed++;
+                }
+                cases.push({
+                    status: status,
+                    name: name,
+                    description: classname,
+                    details: details,
+                    duration: duration
+                });
+            }
+            suites.push({
+                name: testsuite.$.name,
+                timestamp: testsuite.$.timestamp,
+                filename: testsuite.$.file,
+                cases: cases
+            });
+        }
+        return {
+            counts: counts,
+            suites: suites
+        };
+    });
+}
+function parseJunit(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const parser = util.promisify(xml2js_1.default.parseString);
+        const xml = yield parser(data);
+        return yield parseJunitXml(xml);
+    });
+}
+exports.parseJunit = parseJunit;
+function parseTapFile(filename) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const readfile = util.promisify(fs.readFile);
+        return yield parseTap(yield readfile(filename, "utf8"));
+    });
+}
+exports.parseTapFile = parseTapFile;
+function parseJunitFile(filename) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const readfile = util.promisify(fs.readFile);
+        return yield parseJunit(yield readfile(filename, "utf8"));
+    });
+}
+exports.parseJunitFile = parseJunitFile;
+function parseFile(filename) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const readfile = util.promisify(fs.readFile);
+        const parser = util.promisify(xml2js_1.default.parseString);
+        const data = yield readfile(filename, "utf8");
+        if (data.match(/^TAP version 13\r?\n/) ||
+            data.match(/^ok /) ||
+            data.match(/^not ok /)) {
+            return yield parseTap(data);
+        }
+        const xml = yield parser(data);
+        if (xml.testsuites || xml.testsuite) {
+            return yield parseJunitXml(xml);
+        }
+        throw new Error(`unknown test file type for '${filename}'`);
+    });
+}
+exports.parseFile = parseFile;
---
+++ segesbuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This files is used by SEGES (mac@seges.dk) to make local changes to the action
+# We don't want to pollute the main branch and/or the dist branch with our changes, in case we need to update the fork
+# So instead of using the workflow to release on the dist branch, we "release" a dist folder in this branch by running this script locally in WSL
+npm ci
+npm run test
+./node_modules/.bin/tsc 
+./node_modules/.bin/ncc build
+rm -rf dist/
+mkdir dist
+cp action.yml dist/
+cp README.md dist/
+cp LICENSE dist/
+cp bin/* dist/
+rm -rf bin/
+git add -f dist/*
